9013b4c621deded10698c12e69728f6affb57d4c
MichalK96 <miniocha1206@wp.pl>
2022-08-23 14:55:59 +0200

    welcome screen bug with path

--- src/main/java/com/example/crazylab/controlers/SceneController.java
+++ src/main/java/com/example/crazylab/controlers/SceneController.java
@@ -1,31 +1,35 @@
 package com.example.crazylab.controlers;
 
-import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+
+
+
+
+import java.io.IOException;
 
 public class SceneController {
-
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+    @FXML
+    private TextField userName;
 
     @FXML
-    public void switchToScene1(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    void startNewGame(ActionEvent event) throws IOException {
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("gameScreen.fxml"));   //TODO problem ze ścieżką
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
 }
-
-
-
