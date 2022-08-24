895632a6ebd1e26c4578528e1b3ff26b0dfd1119
MichalK96 <miniocha1206@wp.pl>
2022-08-24 10:04:31 +0200

    Bugs

---
+++ src/main/java/com/example/crazylab/controler/SceneController.java
@@ -0,0 +1,36 @@
+package com.example.crazylab.controler;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+
+
+
+
+import java.io.IOException;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private TextField userName;
+
+    @FXML
+    void startNewGame(ActionEvent event) throws IOException {
+        System.out.println(".....................................................");
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("gameScreen.fxml"));   //TODO problem ze ścieżką
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
--- src/main/java/com/example/crazylab/controler/WelcomeScrenControler.java
+++
@@ -1,4 +0,0 @@
-package com.example.crazylab.controler;
-
-public class WelcomeScrenControler {
-}
