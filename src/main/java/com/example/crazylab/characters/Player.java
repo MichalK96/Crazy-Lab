7ad1889bc002f57f834d38053933c871380e01f2
aganowak <agnieszka.nowak@gmx.com>
2022-08-25 13:12:07 +0200

    bug fixed

--- src/main/java/com/example/crazylab/characters/Player.java
+++ src/main/java/com/example/crazylab/characters/Player.java
@@ -18,18 +18,18 @@ public class Player extends Character {
 
     public void addItem(String item) {
         switch (item) {
-            case "mask" -> items.add(new Armour(ArmourType.MASK));
-            case "diyMask" -> items.add(new Armour(ArmourType.DIY_MASK));
-            case "syringe" -> items.add(new Tool(ToolType.SYRINGE));
-            case "staningKit" -> items.add(new Tool(ToolType.STANING_KIT));
-            case "enzymeKit" -> items.add(new Tool(ToolType.ENZYME_KIT));
-            case "usbKey" -> items.add(new Tool(ToolType.USB_KEY));
-            case "dnaSample" -> items.add(new Tool(ToolType.DNA_SAMPLE));
-            case "virusSample" -> items.add(new Tool(ToolType.VIRUS_SAMPLE));
-            case "report" -> items.add(new Tool(ToolType.REPORT));
-            case "microscopeImage" -> items.add(new Tool(ToolType.MICROSCOPE_IMAGE));
-            case "sandwich" -> items.add(new Weapon(WeaponType.SANDWICH));
-            case "spray" -> items.add(new Weapon(WeaponType.SPRAY));
+            case "I_mask" -> items.add(new Armour(ArmourType.MASK));
+            case "I_diyMask" -> items.add(new Armour(ArmourType.DIY_MASK));
+            case "I_syringe" -> items.add(new Tool(ToolType.SYRINGE));
+            case "I_staningKit" -> items.add(new Tool(ToolType.STANING_KIT));
+            case "I_enzymeKit" -> items.add(new Tool(ToolType.ENZYME_KIT));
+            case "I_usbKey" -> items.add(new Tool(ToolType.USB_KEY));
+            case "I_dnaSample" -> items.add(new Tool(ToolType.DNA_SAMPLE));
+            case "I_virusSample" -> items.add(new Tool(ToolType.VIRUS_SAMPLE));
+            case "I_report" -> items.add(new Tool(ToolType.REPORT));
+            case "I_microscopeImage" -> items.add(new Tool(ToolType.MICROSCOPE_IMAGE));
+            case "I_sandwich" -> items.add(new Weapon(WeaponType.SANDWICH));
+            case "I_spray" -> items.add(new Weapon(WeaponType.SPRAY));
             default -> System.out.println("Wrong item type");
         }
 
