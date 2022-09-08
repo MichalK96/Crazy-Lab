57843b444e6f74f6ff78c18b9f28dae79047bd0d
aganowak <agnieszka.nowak@gmx.com>
2022-09-08 11:41:17 +0200

    "FabularObjects enum"

--- src/main/java/com/example/crazylab/FabularObject.java
+++ src/main/java/com/example/crazylab/FabularObject.java
@@ -1,16 +1,18 @@
 package com.example.crazylab;
 
 public enum FabularObject {
-    MICROSCOPE (0,0),
-    SEQUENCER (0,0),
-    SUPERCOMPUTER (0,0),
-    DONOR (0,0);
+    MICROSCOPE (11, 12, 4),
+    SEQUENCER (11, 12, 20),
+    SUPERCOMPUTER (4, 5, 5),
+    DONOR (16, 17, 3);
 
-    private int posX;
-    private int posY;
+    private int x1;
+    private int x2;
+    private int y;
 
-    FabularObject(int x, int y){
-        this.posX = x;
-        this.posY = y;
+    FabularObject(int x1, int x2, int y){
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y = y;
     }
 }
