23232e0b5df7e72083e4295831515872f50a6d8b
aganowak <agnieszka.nowak@gmx.com>
2022-09-08 22:52:44 +0200

    "Armour class added"

---
+++ src/main/java/com/example/crazylab/items/Armour.java
@@ -0,0 +1,32 @@
+package com.example.crazylab.items;
+
+public class Armour extends Item {
+
+    ItemType type;
+
+    private int posX;
+    private int posY;
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public Armour(ItemType type, int posX, int posY) {
+        this.type = type;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public ItemType getType() {
+        return type;
+    }
+
+    @Override
+    public ItemType getItemType() {
+        return this.type;
+    }
+}
