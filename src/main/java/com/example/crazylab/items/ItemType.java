36695b2f8aac876d09546e63b0868c2fc2917498
aganowak <agnieszka.nowak@gmx.com>
2022-09-08 21:09:14 +0200

    "re-start saved game button added"

--- src/main/java/com/example/crazylab/items/ItemType.java
+++ src/main/java/com/example/crazylab/items/ItemType.java
@@ -5,18 +5,18 @@ import java.util.function.Predicate;
 public enum ItemType {
 
     SYRINGE (142, 0, "TOOL", "found_syringe.fxml"),      //random
-    VIRUS_SAMPLE (0, 0, "TOOL", "found_syringe.fxml"), //do zdobycia
+    VIRUS_SAMPLE (0, 0, "TOOL", ""), //do zdobycia
     STANING_KIT (247, 0, "TOOL", "found_stainingKit.fxml"),  // random
     ENZYME_KIT (137, 0, "TOOL", "found_enzymeKit.fxml"),  //random
 //    DNA_SAMPLE (122, 0, "TOOL"),
     USB_KEY (99, 0, "TOOL", "found_USBkey.fxml"),   //random
-    REPORT (0, 0, "TOOL", "found_syringe.fxml"),    //do zdobycia
-    MICROSCOPE_IMAGE (0, 0, "TOOL", "found_syringe.fxml"),  //do zdobycia
-    DNA_SEQUENCE (0, 0, "TOOL", "found_syringe.fxml"),  //do zdobycia
+    REPORT (0, 0, "TOOL", ""),    //do zdobycia
+    MICROSCOPE_IMAGE (0, 0, "TOOL", ""),  //do zdobycia
+    DNA_SEQUENCE (0, 0, "TOOL", ""),  //do zdobycia
     KEY (104, 0, "TOOL", "found_key.fxml"),     //ustawić
     DIY_MASK (124, 0, "ARMOR", "found_mask.fxml"), //random
     ATEST_MASK(114, 0, "ARMOR", "found_mask.fxml"), //random
-    SPRAY(134, 0, "WEAPON", "found_syringe.fxml"),     //random
+    SPRAY(134, 0, "WEAPON", "found_spray.fxml.fxml"),     //random
     SANDWICH(186, 0, "WEAPON", "found_sandwich.fxml");  //random
 
     private final int imageId;
